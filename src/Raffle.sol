47cd89c0f8b38231114b209d69929c7372f49c71
Garo Sanchez <garosanchez27@gmail.com>
2024-12-05 12:06:01 -0600

    Add customError and event

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,3 +1,27 @@
+// Layout of the contract file:
+// version
+// imports
+// errors
+// interfaces, libraries, contract
+
+// Inside Contract:
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+
+// view & pure functions
+
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
@@ -8,13 +32,28 @@ pragma solidity 0.8.19;
  * @dev Implements Chainlink VRFv2.5
  */
 contract Raffle {
+    /* Errors */
+    error Raffle__SendMoreToEnterRaffle();
+
     uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    /* Events */
+    event RaffleEntered(address indexed player);
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
-    function enterRaffle() public payable {}
+    function enterRaffle() public payable {
+        // require(msg.value >= i_entranceFee, "Not enough ETH sent!");
+        if (msg.value < i_entranceFee) {
+            revert Raffle__SendMoreToEnterRaffle();
+        }
+        s_players.push(payable(msg.sender));
+
+        emit RaffleEntered(msg.sender);
+    }
 
     function pickWinner() public {}
 
